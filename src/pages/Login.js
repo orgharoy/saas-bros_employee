ad62bff701e5484aa9010a9d76fbd1dd1625ac02
Orgha Roy <103488415+orgharoy@users.noreply.github.com>
2023-06-22 11:46:15 +0000

    login with api

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -6,15 +6,15 @@ import { UserContext } from '../userContext.js';
 const Login = () => {
   const navigate = useNavigate();
   const { userId, setUserId } = useContext(UserContext);
-  const [formValues, setFormValues] = useState({ email: "", password: ""});
-  const [errorMessages, setErrorMessages] = useState({ email: '', password: '' });
+  const [formValues, setFormValues] = useState({ username: "", password: ""});
+  const [errorMessages, setErrorMessages] = useState({ username: '', password: '' });
 
   const isValid = () => {
-    let errors = { email: '', password: '' };
+    let errors = { username: '', password: '' };
     let isValid = true;
 
-    if (!formValues.email) {
-      errors.email = 'Email is required';
+    if (!formValues.username) {
+      errors.username = 'username is required';
       isValid = false;
     }
     if (!formValues.password) {
@@ -30,13 +30,13 @@ const Login = () => {
     e.preventDefault();
     if (isValid()) {
       try {
-        const response = await axios.post('http://localhost:3001/emp/employee-login', formValues);
-         
-        const { token, employee } = response.data;
-        localStorage.setItem('jwtToken', token);
-        setUserId(employee._id);
+        const response = await axios.post('https://saasproj.bsite.net/api/auth/login', formValues);
+        console.log(response.data);
+        //const { token, employee } = response.data;
+        //localStorage.setItem('jwtToken', token);
+        //setUserId(employee._id);
         
-        navigate('/');
+        //navigate('/');
       } catch (error) {
         console.error(error);
       }
@@ -47,10 +47,10 @@ const Login = () => {
 
     <div className="absolute top-0 left-0 h-screen w-screen flex justify-center items-center bg-teal-300">
 
-      <form className="flex flex-col gap-5 bg-purple-700 p-5 rounded-md text-black">
+      <form className="flex flex-col gap-5 bg-purple-2 p-5 rounded-md text-black">
         <h1 className="text-2xl text-white text-center">Login</h1>
-        <input type="email" placeholder="email" className="p-2 rounded-sm" value={formValues.email} onChange={(e) => setFormValues({...formValues, email:e.target.value})}/>
-        {errorMessages.email && <p className="text-red-600">{errorMessages.email}</p>}
+        <input type="text" placeholder="username" className="p-2 rounded-sm" value={formValues.username} onChange={(e) => setFormValues({...formValues, username:e.target.value})}/>
+        {errorMessages.username && <p className="text-red-600">{errorMessages.username}</p>}
         <input type="password" placeholder="password" className="p-2 rounded-sm" value={formValues.password} onChange={(e) => setFormValues({...formValues, password:e.target.value})}/>
         {errorMessages.password && <p className="text-red-600"> {errorMessages.password} </p>}
         <button className="bg-white text-purple-600 rounded-md hover:bg-teal-300 hover:text-white" onClick={handleSubmit} >Login</button>
