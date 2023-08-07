0b6cca2325e8077b7c426a305cde7f79c09c6c83
Orgha Roy <103488415+orgharoy@users.noreply.github.com>
2023-08-07 08:53:23 +0000

    editing users

--- src/pages/User.js
+++ src/pages/User.js
@@ -125,32 +125,3 @@ const User = () => {
 }
 
 export default User
-
-
-
-  // <div className="bg-white">
-  //     <h1>User Details Page</h1>
-
-  //     {user.user.length > 0 ? (
-  //       <>
-  //         <h1>user ID: {user.user[0]._id}</h1>
-  //         <h1>business Name: {user.user[0].businessName}</h1>
-  //         <h1>business Type: {user.user[0].businessType}</h1>
-  //         <h1>owner name: {user.user[0].ownerName}</h1>
-  //         <h1>owner Contact: {user.user[0].ownerContactNumber}</h1>
-  //         <h1>business Email: {user.user[0].businessEmail}</h1>
-  //         <h1>Business Contact Number: {user.user[0].businessContactNumber}</h1>
-  //         <h1>Business Address: {user.user[0].businessAddress}</h1>
-  //         <h1>Business Registration Number: {user.user[0].businessRegNumber}</h1>
-  //         <h1>Business Tax Id: {user.user[0].businessTaxId}</h1>
-  //         <h1>Number Of Employees: {user.user[0].numberOfEmployees}</h1>
-  //         <h1>Number Of Employees: {user.user[0].numberOfEmployees}</h1>
-  //         <h1>Sales Volume: {user.user[0].salesVolume}</h1>
-  //         <h1>Created By: {user.user[0].createdBy}</h1>
-  //         <h1>Created At: {user.user[0].createdAt}</h1>
-  //         <h1>Updated By: {user.user[0].updatedAt}</h1>
-  //       </>
-  //     ) : (
-  //       <h1>Loading...</h1>
-  //     )}
-  //   </div>
