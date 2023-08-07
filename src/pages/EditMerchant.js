b250dd59cd8eb8f3e0a60540305961f542798e3b
Orgha Roy <orgharoy@gmail.com>
2023-08-07 07:55:53 +0600

    minor changes

--- src/pages/EditMerchant.js
+++ src/pages/EditMerchant.js
@@ -1,6 +1,34 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react';
+import {Link, useParams} from 'react-router-dom';
+import axios from 'axios';
 
 const EditMerchant = () => {
+  const userId = useParams();
+  const [merchant, setMerchant] = useState([]);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+
+      const token = localStorage.getItem('RepeatifyToken');
+
+      try {
+        const response = await axios.get(`https://saasproj.bsite.net/api/admin/merchant/${userId.id}`,{
+          headers: {
+            Authorization: `Bearer ${token}`
+          }
+        })
+
+        setMerchant(response.data);
+        
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchUsers();
+  }, [userId]);
+
+  
   return (
     <div>EditMerchant</div>
   )
